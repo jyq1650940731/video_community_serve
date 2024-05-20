7028b3c2569156ac1796303424a41d7621e25461
jyq1650940731 <69760232+jyq1650940731@users.noreply.github.com>
2024-05-20 14:11:43 +0800

    init

---
+++ src/main/java/com/jyqhome/balaparent/BalaParentApplication.java
@@ -0,0 +1,13 @@
+package com.jyqhome.balaparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalaParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalaParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jyqhome/balaparent/BalaParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyqhome.balaparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalaParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
